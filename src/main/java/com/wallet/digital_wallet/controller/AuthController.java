ce6344e791eef20dfe057f8234bbd0fee8f367b8
Hadiyal007 <harshilhadiyal2006@gmail.com>
2026-03-25 17:59:49 +0530

    [IMP]add AuthController with /me endpoint and DataInitializer for demo data

--- src/main/java/com/wallet/digital_wallet/controller/AuthController.java
+++ src/main/java/com/wallet/digital_wallet/controller/AuthController.java
@@ -1,12 +1,15 @@
 package com.wallet.digital_wallet.controller;
 
-import com.wallet.digital_wallet.dto.ApiResponse;
+
+
 import com.wallet.digital_wallet.entity.User;
+import com.wallet.digital_wallet.dto.ApiResponse;
 import com.wallet.digital_wallet.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/auth")
